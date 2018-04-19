f0058d8b8035610e3e9fa407a9e0edab4bf2d983
Priya Hiremath <priyahrish18@gmail.com>
2018-04-19 20:35:53 +1200

    first-commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const server = require('./server')
 
 const port = process.env.PORT || 3000
 
-server.listen(port, () => {
+server.listen(port, '0.0.0.0', () => {
   // eslint-disable-next-line no-console
   console.log('Listening on port', port)
 })
--- server/routes/getCookies.js
+++ server/routes/getCookies.js
@@ -2,10 +2,25 @@ const express = require('express')
 const router = express.Router()
 
 // const db = require('../db')
+const monsters = [
+    {
+        id: 1,
+         name:"Elmo",
+         image:""
+    },
+    {
+        id:2,
+         name:"Cookie monster",
+          image:""
+        }
+]
 
 router.get('/', function (req, res) {
 //   res.json(db.getWidgets())
+    res.json(monsters)
+
 })
+
 
 
 module.exports = router
