c5e43b7332909f06158c7e8afa3dd967b5425b6b
anugrah danang abiyoga <danangabiyoga26@gmail.com>
2018-04-20 13:50:10 +1200

    style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -36,8 +36,10 @@ export default class App extends React.Component {
 render() {
     return (
         <div>
-            <div className={`${this.state.playing ? 'spinning' : ''} spinner`}> <img className='cookie' src='http://priya-sesame-street-server.herokuapp.com/images/cookie.png'/></div>
-            <div> 
+            <div className={`${this.state.playing ? 'spinning' : ''} spinner`}> 
+                <img className='cookie' src='http://priya-sesame-street-server.herokuapp.com/images/cookie.png'/>
+            </div>
+            <div className={`${this.state.playing ? 'slide-top' : ''} spinner`}>
                 {this.state.images.length !== 0 && <CookieMonster imageUrl={this.state.images[1].imageUrl}/>}
                 <SongButton playing={this.state.playing}  startPlaying={this.startPlaying} />
             </div>
